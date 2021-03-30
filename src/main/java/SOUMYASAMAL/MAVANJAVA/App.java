26e5948c246aea1f9bd25ad5f9f11abc250982d9
Guru Prasad Das <gurupdas11@gmail.com>
2021-03-30 16:35:06 +0530

    munu

---
+++ src/main/java/SOUMYASAMAL/MAVANJAVA/App.java
@@ -0,0 +1,13 @@
+package SOUMYASAMAL.MAVANJAVA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SOUMYASAMAL/MAVANJAVA/som.java
@@ -0,0 +1,16 @@
+package SOUMYASAMAL.MAVANJAVA;
+
+import org.testng.annotations.Test;
+
+public class som {
+
+	@Test
+	public void MunuTesting()
+	{
+		System.out.println("MunuTesting");
+	}
+
+
+	}
+
+
